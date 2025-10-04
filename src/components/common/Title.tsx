c97e28b5c19bea13678969d9627b195742dfe7ac
Dawid Badurek <dawidbadurek@wp.pl>
2025-10-04 20:51:53 +0200

    Update Title

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -3,7 +3,7 @@ interface TitleProps {
 }
 
 function Title({ text }: TitleProps) {
-  return <h2 className="text-2xl pb-2">{text}</h2>;
+  return <h2 className="text-3xl pb-2">{text}</h2>;
 }
 
 export default Title;
