49785f36594b219f1348640b48aa5e9ccd22a4d6
konrad <realagmag@op.pl>
2025-10-05 01:11:30 +0200

    Add event

--- src/game/events/event-manager.ts
+++ src/game/events/event-manager.ts
@@ -7,6 +7,7 @@ import { VegateblesGetMoreExpensive } from "./list/vegatebles-get-more-expensive
 import { FriendsByReading } from "./list/friends-by-reading";
 import { HelpedNeighbor } from "./list/helped-neighbour.ts";
 import { CarIsBroken } from "./list/car-is-broken.ts";
+import {TravelingDueToCar} from "./list/traveling-due-to-car.ts";
 
 export class EventManager {
   private events: Event[];
@@ -20,6 +21,7 @@ export class EventManager {
       new FriendsByReading(),
       new HelpedNeighbor(),
       new CarIsBroken(),
+      new TravelingDueToCar(),
     ];
   }
 
