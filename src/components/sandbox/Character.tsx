dcb853a3fdd99cb82bd00ed6fa8b2b898798802f
Alan <alan.sipiorski14@gmail.com>
2025-10-05 01:44:11 +0200

    Add character avatar
    # Conflicts:
    #	src/components/sandbox/Character.tsx

--- src/components/sandbox/Character.tsx
+++ src/components/sandbox/Character.tsx
@@ -1,5 +1,9 @@
 import Title from "../common/Title.tsx";
 import type { GameTickResult } from "../../game/game.ts";
+import eclipse from "../../assets/eclipse.png";
+
+// FIXME: should be changed to dynamic character based on user choice
+import character from "../../assets/character/character_male_18.png";
 
 interface CharacterProps {
   tickResult: GameTickResult;
@@ -9,6 +13,12 @@ function Character({ tickResult }: CharacterProps) {
   return (
     <>
       <Title text={`Age ${tickResult.state.age}`} />
+      <img src={character} alt="Character" />
+      <img
+        src={eclipse}
+        alt="Character"
+        className="w-32 h-32 mx-auto transform scale-y-50 translate-y-[-4.5rem]"
+      />
     </>
   );
 }
