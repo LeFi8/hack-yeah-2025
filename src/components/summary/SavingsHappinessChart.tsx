d178ab4b4e18c7fbe6bb2c364403058f6dcdbdd5
Alan <alan.sipiorski14@gmail.com>
2025-10-04 21:55:58 +0200

    Add nivo summary charts

--- src/components/summary/SavingsHappinessChart.tsx
+++ src/components/summary/SavingsHappinessChart.tsx
@@ -1,12 +1,57 @@
 import Title from "../common/Title.tsx";
+import { ResponsiveLine } from "@nivo/line";
 
+// FIXME: mocked data
+const data = [
+  {
+    id: "savings",
+    data: [
+      { x: "18", y: 3000 },
+      { x: "19", y: 3800 },
+      { x: "20", y: 3200 },
+      { x: "21", y: 5000 },
+    ],
+  },
+  {
+    id: "happiness",
+    data: [
+      { x: "18", y: 5000 },
+      { x: "19", y: 2500 },
+      { x: "20", y: 4000 },
+      { x: "21", y: 1000 },
+    ],
+  }
+];
 
 function SavingsHappinessChart() {
   return (
     <>
       <Title text={"Savings & Happiness"} />
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 20, right: 10, bottom: 60, left: 10 }}
+        yScale={{
+          type: "linear",
+          min: 0,
+          max: "auto",
+          stacked: false,
+          reverse: false,
+        }}
+        curve="natural"
+        colors={{ scheme: "set2" }}
+        enableArea={true}
+        axisLeft={null}
+        axisBottom={{ legend: "Age", legendOffset: 20, legendPosition: "middle" }}
+        pointSize={10}
+        pointColor={{ theme: "background" }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "seriesColor" }}
+        enableGridX={false}
+        enableGridY={false}
+        useMesh={true}
+      />
     </>
-  )
+  );
 }
 
 export default SavingsHappinessChart;
--- src/routes/Summary.tsx
+++ src/routes/Summary.tsx
@@ -6,39 +6,41 @@ import InfoTile from "../components/summary/InfoTile.tsx";
 function Summary() {
   return (
     <>
-      <div className="flex flex-col gap-5 p-4 h-dvh">
-        <div className="flex flex-row gap-5 flex-1">
-          <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-4">
-            <SavingsHappinessChart />
-          </div>
-          <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
-            <FocusChart />
-          </div>
-        </div>
-        <div className="flex flex-row gap-5 flex-2">
-          <div className="flex flex-col gap-5 flex-2">
-            <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
-              <InfoTile title="Pension" value="5,000 PLN" />
-            </div>
-            <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-6">
-              TODO: Items
-            </div>
-          </div>
-          <div className="flex flex-col flex-4 gap-5">
-            <div className="flex flex-row gap-5">
-              <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
-                <InfoTile title="ZUS Account" value="100,000 PLN" />
-              </div>
-              <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
-                <InfoTile title="Replacement Rate" value="75%" />
-              </div>
+      <div className="h-dvh">
+        <div className="flex flex-col gap-5 p-4 h-full">
+          <div className="flex flex-row gap-5 flex-1">
+            <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-4">
+              <SavingsHappinessChart />
             </div>
             <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
-              TODO: Choices
+              <FocusChart />
             </div>
           </div>
-          <div className="border-2 rounded-2xl bg-white shadow-md flex-3 p-5">
-            <Title text="Your Life" />
+          <div className="flex flex-row gap-5 flex-2">
+            <div className="flex flex-col gap-5 flex-1">
+              <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
+                <InfoTile title="Pension" value="5,000 PLN" />
+              </div>
+              <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-6">
+                TODO: Items
+              </div>
+            </div>
+            <div className="flex flex-col flex-4 gap-5">
+              <div className="flex flex-row gap-5">
+                <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
+                  <InfoTile title="ZUS Account" value="100,000 PLN" />
+                </div>
+                <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
+                  <InfoTile title="Replacement Rate" value="75%" />
+                </div>
+              </div>
+              <div className="bg-white shadow-md py-5 px-8 border-2 rounded-2xl flex-1">
+                TODO: Choices
+              </div>
+            </div>
+            <div className="border-2 rounded-2xl bg-white shadow-md flex-3 p-5">
+              <Title text="Your Life" />
+            </div>
           </div>
         </div>
       </div>
