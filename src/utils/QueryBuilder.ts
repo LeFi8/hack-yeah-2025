385a9e218bd8dfbe62e8051a9eef41e8c8dd7af0
Alan <alan.sipiorski14@gmail.com>
2025-10-04 23:29:35 +0200

    Extend AI query to 200 words

--- src/utils/QueryBuilder.ts
+++ src/utils/QueryBuilder.ts
@@ -1,6 +1,6 @@
 class QueryBuilder {
   private static readonly queryHelper =
-    "Summarize my life in 180 words based on the following aspects and data. " +
+    "Summarize my life in 200 words based on the following aspects and data. " +
     "Use human language (do not make it sound like AI, or use overly complicated words, make it easy to read) and make it engaging. " +
     "Do not repeat the aspects. Make it sound like a story. " +
     "Aspects and data: ";
