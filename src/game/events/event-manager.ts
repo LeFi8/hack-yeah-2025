5cde13ff6bac1a70242e97a1b70bb77feb0913b4
Alex <aleksandr.birukov@besidethepark.com>
2025-10-05 03:07:54 +0200

    Add health check event.

--- src/game/events/event-manager.ts
+++ src/game/events/event-manager.ts
@@ -8,15 +8,15 @@ import { FriendsByReading } from "./list/friends-by-reading";
 import { TravelingDueToCar } from "./list/traveling-due-to-car";
 import { EducationLvlUpgrade } from "./list/education-lvl-upgrade";
 import { WorkLvlUpgrade } from "./list/work-lvl-upgrade";
-import {HelpedNeighbor} from "./list/helped-neighbour.ts";
-import {CarIsBroken} from "./list/car-is-broken.ts";
-import {BreakUp} from "./list/break-up.ts";
+import { HelpedNeighbor } from "./list/helped-neighbour.ts";
+import { CarIsBroken } from "./list/car-is-broken.ts";
+import { BreakUp } from "./list/break-up.ts";
 
 export type EventHistory = {
   event: Event;
   month: number;
   age: number;
-}
+};
 export class EventManager {
   private history: EventHistory[] = [];
   private DEACREASED_CHANCE_IF_HAPPENED = 5;
@@ -45,7 +45,6 @@ export class EventManager {
     ];
   }
 
-
   // If event already happen we reduce chance of it happening again
   getRandom(state: State): Event | null {
     const filteredEvents = this.getAllEvents(state).filter((event) =>
@@ -58,22 +57,23 @@ export class EventManager {
 
     const weightedEvents = filteredEvents
       .map((event) => {
-      let weight = event.getWeight();
-      
-      // Check if this type of event has already happened
-      const hasHappened = this.history.some(historyEntry => 
-        historyEntry.event.constructor === event.constructor
-      );
-      
-      if (hasHappened) {
-        weight = weight / this.DEACREASED_CHANCE_IF_HAPPENED;
-      }
-      
-      return {
-        event,
-        weight: weight
-      };
-    })
+        let weight = event.getWeight();
+
+        // Check if this type of event has already happened
+        const hasHappened = this.history.some(
+          (historyEntry) =>
+            historyEntry.event.constructor === event.constructor,
+        );
+
+        if (hasHappened) {
+          weight = weight / this.DEACREASED_CHANCE_IF_HAPPENED;
+        }
+
+        return {
+          event,
+          weight: weight,
+        };
+      })
       .filter((item) => item.weight > 0);
 
     if (weightedEvents.length === 0) {
--- src/game/items/list/credit.ts
+++ src/game/items/list/credit.ts
@@ -1,20 +1,20 @@
-import type {Item} from "../item.ts";
-import type {State} from "../../state.ts";
+import type { Item } from "../item.ts";
+import type { State } from "../../state.ts";
 
 export class Credit implements Item {
-    monthlyCost: number = 0;
-    monthsLeft: number = 0;
-    constructor(monthlyPayment: number, months: number) {
-        this.monthlyCost = monthlyPayment;
-        this.monthsLeft = months;
+  monthlyCost: number = 0;
+  monthsLeft: number = 0;
+  constructor(monthlyPayment: number, months: number) {
+    this.monthlyCost = monthlyPayment;
+    this.monthsLeft = months;
+  }
+  applyMonthlyEffects(state: State) {
+    this.monthsLeft--;
+    if (this.monthsLeft < 0) {
+      const credit = state.items.find((item: Item) => item === this);
+      if (credit) {
+        state.removeItem(credit);
+      }
     }
-    applyMonthlyEffects(state: State) {
-        this.monthsLeft--;
-        if (this.monthsLeft < 0) {
-            const credit = state.items.find((item: Item) => item === this)
-            if (credit) {
-                state.removeItem(credit);
-            }
-        }
-    }
+  }
 }
