235aec2edf28d3c343db11fa75ac127158259e15
Szymon Kruk <szymon.kruk@besidethepark.com>
2025-10-05 09:41:22 +0200

    Add FocusTracker

--- src/game/work/list/worker.ts
+++ src/game/work/list/worker.ts
@@ -11,8 +11,7 @@ export class Worker extends JobContract {
   }
 
   getBruttoIncome(): number {
-    const multiplier = 1 + this.state.education.level.get() / 5;
-    return 5000 * this.lvl * multiplier;
+    return 5000 * (1.2 ** this.lvl) ;
   }
 
   getPosition(): string {
