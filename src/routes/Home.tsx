9e7d1302cdd983339a0ede3690361b077721e548
Alan <alan.sipiorski14@gmail.com>
2025-10-05 05:08:31 +0200

    Refactor home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,53 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import { Paths } from "../constants/paths.ts";
 
 function Home() {
+  const navigate = useNavigate();
   return (
-    <>
-      <h1 className="text-center w-full">Home</h1>
-      <div className="flex justify-center p-4">
-        <Link className="" to={Paths.SandboxMode}>
-          <div className="p-4 bg-red-300 w-fit">
-            Rozpocznij grę! (tryb swobodny)
+    <div className="min-h-screen bg-custom-yellow">
+      <div className="bg-gradient-to-b from-zus/30 to-transparent"></div>
+      <div className="max-w-6xl mx-auto px-4 py-16 text-center">
+        <div className="animate-show">
+          <h1 className="text-6xl md:text-8xl font-bold text-zus mb-6">
+            <span className="text-black">Five</span>LifesRun
+          </h1>
+        </div>
+        <p className="text-black mt-6 max-w-96 mx-auto flex items-center justify-center text-center">
+          Learn about the impact of your lifestyle choices on your health,
+          finances, happiness, and many other aspects of life.
+        </p>
+
+        <div className="flex flex-col gap-4 mt-24">
+          <div
+            className="cursor-pointer shadow-lg p-4 rounded-lg border-1 hover:scale-105 transition-transform duration-200 ease-in-out bg-white"
+            onClick={() => {
+              navigate(Paths.SandboxMode);
+            }}
+          >
+            Sandbox Mode
           </div>
-        </Link>
+          <div className="relative shadow-lg p-4 rounded-lg border-1 hover:scale-105 transition-transform duration-200 ease-in-out bg-white">
+            Challenges Mode
+            <span className="absolute top-2 right-2 bg-red-400 text-xs text-black px-2 py-0.5 rounded font-semibold italic pointer-events-none select-none">
+              Coming Soon
+            </span>
+          </div>
+          <div className="relative shadow-lg p-4 rounded-lg border-1 hover:scale-105 transition-transform duration-200 ease-in-out bg-white">
+            Story Mode
+            <span className="absolute top-2 right-2 bg-red-400 text-xs text-black px-2 py-0.5 rounded font-semibold italic pointer-events-none select-none">
+              Coming Soon
+            </span>
+          </div>
+          {/* TODO: on click it navigates to the github repository README file */}
+          <div
+            className="cursor-pointer shadow-lg p-4 rounded-lg border-1 hover:scale-105 transition-transform duration-200 ease-in-out bg-zus/60"
+            onClick={() => {}}
+          >
+            About the Game
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
