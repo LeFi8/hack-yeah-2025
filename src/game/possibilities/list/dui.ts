0d1e3d6bc8452e7e1124e33f60bc89ae983b8229
konrad <realagmag@op.pl>
2025-10-05 09:34:21 +0200

    Fix bug

--- src/game/possibilities/list/dui.ts
+++ src/game/possibilities/list/dui.ts
@@ -1,20 +1,21 @@
 import { Possibility } from "../possibility";
 import { Car } from "../../items/list/car.ts";
 import { ArrestWarrant } from "../../items/list/arrest-warrant.ts";
+import type {Item} from "../../items";
 
 export class DrivingUnderInfluence extends Possibility {
   title = "Come home from party";
   getOptions() {
     return [
       {
-        title: "Drive home - you've had some beers",
+        title: "Drive home (you've had some beers)",
         applyEffects: () => {
           this.state.addItem(new ArrestWarrant());
           this.state.character.physicalHealth.add(-5);
         },
       },
       {
-        title: "You take a taxi ",
+        title: "Take a taxi ",
         applyEffects: () => {
           this.state.character.balance -= 20;
         },
@@ -22,7 +23,7 @@ export class DrivingUnderInfluence extends Possibility {
     ];
   }
   canActivate() {
-    return this.state.items.some((i) => i instanceof Car);
+    return this.state.items.some((i: Item) => i instanceof Car) && !this.state.items.some((i: Item) => i instanceof ArrestWarrant);
   }
   getWeight() {
     return 1;
