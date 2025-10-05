edc179bd383d7568c9dd6d11e11bca324e6934dd
Alan <alan.sipiorski14@gmail.com>
2025-10-05 05:12:27 +0200

    Modify coming soon labels

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,7 +4,7 @@ import { Paths } from "../constants/paths.ts";
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="min-h-screen bg-custom-yellow">
+    <div className="min-h-screen bg-gradient-to-b from-bg-custom-yellow to-white">
       <div className="bg-gradient-to-b from-zus/30 to-transparent"></div>
       <div className="max-w-6xl mx-auto px-4 py-16 text-center">
         <div className="animate-show">
@@ -28,13 +28,13 @@ function Home() {
           </div>
           <div className="relative shadow-lg p-4 rounded-lg border-1 hover:scale-105 transition-transform duration-200 ease-in-out bg-white">
             Challenges Mode
-            <span className="absolute top-2 right-2 bg-red-400 text-xs text-black px-2 py-0.5 rounded font-semibold italic pointer-events-none select-none">
+            <span className="absolute top-2 right-2 transform translate-x-6 bg-red-400 text-xs text-black px-2 py-0.5 rounded font-semibold italic pointer-events-none select-none">
               Coming Soon
             </span>
           </div>
           <div className="relative shadow-lg p-4 rounded-lg border-1 hover:scale-105 transition-transform duration-200 ease-in-out bg-white">
             Story Mode
-            <span className="absolute top-2 right-2 bg-red-400 text-xs text-black px-2 py-0.5 rounded font-semibold italic pointer-events-none select-none">
+            <span className="absolute top-2 right-2 transform translate-x-6 bg-red-400 text-xs text-black px-2 py-0.5 rounded font-semibold italic pointer-events-none select-none">
               Coming Soon
             </span>
           </div>
