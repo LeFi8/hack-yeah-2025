af6c9f382802f2e88d302d40f3b7e4138ef229f9
Dawid Badurek <dawidbadurek@wp.pl>
2025-10-05 03:04:01 +0200

    Update StateBar

--- src/components/sandbox/state/StateBar.tsx
+++ src/components/sandbox/state/StateBar.tsx
@@ -8,7 +8,8 @@ interface StateBarProps {
 }
 
 function StateBar({ title, icon, value }: StateBarProps) {
-  const width = Math.min(100, value);
+  let width = Math.min(100, value);
+  width = Math.max(10, width);
 
   const r = Math.max(0, 255 - value * 2.55);
   const g = Math.min(255, value * 2.55);
@@ -20,15 +21,20 @@ function StateBar({ title, icon, value }: StateBarProps) {
       <div className="flex gap-2 items-center">
         {icon}
         <div className={classNames("rounded-2xl h-6 relative grow border-1")}>
-          <div
-            className={classNames(
-              "h-6 rounded-2xl absolute top-[-1px] border-y-1",
-              {
-                "border-r-1": width < 100,
-              },
-            )}
-            style={{ width: `${width}%`, background: `rgb(${r}, ${g}, ${b})` }}
-          ></div>
+          {!!width && (
+            <div
+              className={classNames(
+                "h-6 rounded-2xl absolute top-[-1px] border-y-1",
+                {
+                  "border-r-1": width < 100,
+                },
+              )}
+              style={{
+                width: `${width}%`,
+                background: `rgb(${r}, ${g}, ${b})`,
+              }}
+            ></div>
+          )}
         </div>
         <p className="text-right w-[5ch]">{value}%</p>
       </div>
